fc58e90b9fcfaf3bb0cb7cbb82660dd7d45d4b72
Supergamerrr <83313652+Supergamerrr@users.noreply.github.com>
2022-10-16 02:35:30 +0300

    rttre

---
+++ page1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<link href="Untitled1.css" rel="stylesheet">
+<link href="page1.css" rel="stylesheet">
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:441px;top:967px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_Image1" style="position:absolute;left:0px;top:0px;width:1114px;height:626px;z-index:1;">
+<img src="images/L07BzKR.jpg" id="Image1" alt="" width="1114" height="627"></div>
+<div id="Layer1" style="position:absolute;text-align:left;left:0px;top:587px;width:1114px;height:39px;z-index:2;">
+</div>
+
+<div id="wb_Captcha1" style="position:absolute;left:340px;top:206px;width:145px;height:100px;z-index:4;">
+<img src="captcha1.php" alt="Click for new image" title="Click for new image" style="cursor:pointer;float:left;width:100px;height:38px;" onclick="this.src='captcha1.php?'+Math.random()">
+<span><input type="text" id="Captcha1" style="margin-top:12px;width:calc(100% - 10px);height:16px;" name="captcha" value="" spellcheck="false"></span></div>
+</body>
+</html>
